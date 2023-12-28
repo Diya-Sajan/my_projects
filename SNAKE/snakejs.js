3abf691f2f3cca24976f8581a30a1a60a419f370
Diya Sajan <diyasajan.2301@gmail.com>
2023-12-28 12:32:34 +0530

    making snake game

---
+++ SNAKE/snakejs.js
@@ -0,0 +1,127 @@
+const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BACKGROUND_COLOUR = "white";
+const SNAKE_COLOUR = 'lightgreen';
+const SNAKE_BORDER_COLOUR = 'darkgreen';
+let score = 0;
+
+let snake = [
+    {x: 150, y: 150},
+    {x: 140, y: 150},
+    {x: 130, y: 150},
+    {x: 120, y: 150},
+    {x: 110, y: 150}
+]
+// Horizontal velocity
+let dx = 10;
+// Vertical velocity
+let dy = 0;
+
+// Get the canvas element
+var gameCanvas = document.getElementById("gameCanvas");
+
+// Return a two dimensional drawing context
+var ctx = gameCanvas.getContext("2d");
+
+//  Select the colour to fill the canvas
+ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+//  Select the colour for the border of the canvas
+ctx.strokestyle = CANVAS_BORDER_COLOUR;
+
+// Draw a "filled" rectangle to cover the entire canvas
+ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+// Draw a "border" around the entire canvas
+ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+createFood()
+main()
+
+function drawSnake() {
+    // loop through the snake parts drawing each part on the canvas
+    snake.forEach(drawSnakePart)
+}
+
+function drawSnakePart(snakePart) {
+    // Set the colour of the snake part
+    ctx.fillStyle = SNAKE_COLOUR;
+
+    // Set the border colour of the snake part
+    ctx.strokestyle = SNAKE_BORDER_COLOUR;
+
+    // Draw a "filled" rectangle to represent the snake part at the coordinates
+    // the part is located
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+
+    // Draw a border around the snake part
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+function changeDirection(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+    if (keyPressed === LEFT_KEY && !goingRight) {
+      dx = -10;
+      dy = 0;
+    }
+    if (keyPressed === UP_KEY && !goingDown) {
+      dx = 0;
+      dy = -10;
+    }
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+      dx = 10;
+      dy = 0;
+    }
+    if (keyPressed === DOWN_KEY && !goingDown) {
+      dx = 0;
+      dy = 10;
+    }
+}
+
+function advanceSnake() {
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    snake.unshift(head);
+    snake.pop();
+}
+
+function clearCanvas() {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
+function main() {
+    setTimeout(function onTick() {
+      clearCanvas();
+      drawFood();
+      advanceSnake();
+      drawSnake();
+      // Call main again
+      main();
+    }, 100)
+}
+
+document.addEventListener("keydown", changeDirection)
+
+function randomTen(min, max) {
+    return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+}
+  function createFood() {
+    foodX = randomTen(0, gameCanvas.width - 10);
+    foodY = randomTen(0, gameCanvas.height - 10);
+    snake.forEach(function isFoodOnSnake(part) {
+      const foodIsOnSnake = part.x == foodX && part.y == foodY
+      if (foodIsOnSnake)
+        createFood();
+    });
+}
+function drawFood() {
+    ctx.fillStyle = 'red';
+    ctx.strokestyle = 'darkred';
+    ctx.fillRect(foodX, foodY, 10, 10);
+    ctx.strokeRect(foodX, foodY, 10, 10);
+}
