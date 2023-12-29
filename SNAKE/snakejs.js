db195e9b19e8c609cd7f67d9c068902e48342665
Diya Sajan <diyasajan.2301@gmail.com>
2023-12-29 16:18:32 +0530

    finished basic snake game using js and html

--- SNAKE/snakejs.js
+++ SNAKE/snakejs.js
@@ -15,6 +15,13 @@ let snake = [
 let dx = 10;
 // Vertical velocity
 let dy = 0;
+// When set to true the snake is changing direction
+let changingDirection = false;
+// Food x-coordinate
+let foodX;
+// Food y-coordinate
+let foodY;
+
 
 // Get the canvas element
 var gameCanvas = document.getElementById("gameCanvas");
@@ -59,33 +66,51 @@ function changeDirection(event) {
     const RIGHT_KEY = 39;
     const UP_KEY = 38;
     const DOWN_KEY = 40;
+
+    if (changingDirection) return;
+    changingDirection = true;
+
     const keyPressed = event.keyCode;
+
     const goingUp = dy === -10;
     const goingDown = dy === 10;
     const goingRight = dx === 10;
     const goingLeft = dx === -10;
     if (keyPressed === LEFT_KEY && !goingRight) {
-      dx = -10;
-      dy = 0;
+        dx = -10;
+        dy = 0;
     }
     if (keyPressed === UP_KEY && !goingDown) {
-      dx = 0;
-      dy = -10;
+        dx = 0;
+        dy = -10;
     }
     if (keyPressed === RIGHT_KEY && !goingLeft) {
-      dx = 10;
-      dy = 0;
+        dx = 10;
+        dy = 0;
     }
     if (keyPressed === DOWN_KEY && !goingDown) {
-      dx = 0;
-      dy = 10;
+        dx = 0;
+        dy = 10;
     }
 }
 
 function advanceSnake() {
     const head = {x: snake[0].x + dx, y: snake[0].y + dy};
     snake.unshift(head);
-    snake.pop();
+
+    const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+        if (didEatFood) {
+            // Increase score
+            score += 10;
+            // Display score on screen
+            document.getElementById('score').innerHTML = score;
+
+            // Generate new food location
+            createFood();
+        } else {
+            // Remove the last part of snake body
+            snake.pop();
+        }
 }
 
 function clearCanvas() {
@@ -95,13 +120,15 @@ function clearCanvas() {
     ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
 function main() {
+    if (didGameEnd()) return;
     setTimeout(function onTick() {
-      clearCanvas();
-      drawFood();
-      advanceSnake();
-      drawSnake();
-      // Call main again
-      main();
+        changingDirection = false;
+        clearCanvas();
+        drawFood();
+        advanceSnake();
+        drawSnake();
+        // Call main again
+        main();
     }, 100)
 }
 
@@ -124,4 +151,19 @@ function drawFood() {
     ctx.strokestyle = 'darkred';
     ctx.fillRect(foodX, foodY, 10, 10);
     ctx.strokeRect(foodX, foodY, 10, 10);
+}
+function didGameEnd() {
+    for (let i = 4; i < snake.length; i++) {
+      const didCollide = snake[i].x === snake[0].x &&
+        snake[i].y === snake[0].y
+      if (didCollide) return true
+    }
+    const hitLeftWall = snake[0].x < 0;
+    const hitRightWall = snake[0].x > gameCanvas.width - 10;
+    const hitToptWall = snake[0].y < 0;
+    const hitBottomWall = snake[0].y > gameCanvas.height - 10;
+    return hitLeftWall || 
+           hitRightWall || 
+           hitToptWall ||
+           hitBottomWall
 }
